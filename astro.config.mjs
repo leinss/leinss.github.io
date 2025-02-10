4a633106c288b73fe4451b92a011c5125887757f
Tobias Leinss <tobias@leinss.xyz>
2025-02-10 05:42:52 +0000

    Update deps, Add spotlight

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
-import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config"
+import mdx from "@astrojs/mdx"
+import sitemap from "@astrojs/sitemap"
+import tailwind from "@astrojs/tailwind"
+
+import sentry from "@sentry/astro"
+import spotlightjs from "@spotlightjs/astro"
 
 export default defineConfig({
   site: "https://leinss.github.io",
   // base: "/astro-sphere",
   // trailingSlash: "always",
-  integrations: [mdx(), sitemap(), tailwind()],
-});
+  integrations: [mdx(), sitemap(), tailwind(), sentry(), spotlightjs()],
+})
